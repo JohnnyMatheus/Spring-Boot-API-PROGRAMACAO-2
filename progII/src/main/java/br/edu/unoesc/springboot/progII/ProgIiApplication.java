f33c8184eb1f187df36f53bd396c6f949c6e469c
Johnny Matheus <alunotioficial@gmail.com>
2024-10-31 16:50:43 -0300

    Criando projeto
    Criado o projeto usando o https://start.spring.io/

---
+++ progII/src/main/java/br/edu/unoesc/springboot/progII/ProgIiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.springboot.progII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgIiApplication.class, args);
+	}
+
+}
---
+++ progII/src/test/java/br/edu/unoesc/springboot/progII/ProgIiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.springboot.progII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
